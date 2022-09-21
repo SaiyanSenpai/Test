180e40d02847a340944c4de412d74d4d26a06052
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-09-21 16:53:30 -0400

    Update Week3.py

--- Week3.py
+++ Week3.py
@@ -2,5 +2,12 @@
 print('---Lab Challenge 1---')
 
 Courses = ["ET710","ET574","ENG310",]
+print(Courses)
 print(Courses[2])
 print(Courses[0])
+
+print('---Lab Challenge 2---')
+del Courses[0]
+del Courses[1]
+print(Courses)
+print(f'I am taking {len(Courses)} courses.')
