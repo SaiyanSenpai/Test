9f28b748f83fdf15f4b85bec6043ea01ae47abbb
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-09-21 17:28:34 -0400

    Update Week3.py

--- Week3.py
+++ Week3.py
@@ -1,25 +1,25 @@
 #Week 3 Practice Lists
 print('---Lab Challenge 1---')
 
-Courses = ["ET710","ET574","ENG310",]
-print(Courses)
-print(Courses[2])
-print(Courses[0])
+courses = ["ET710","ET574","ENG310",]
+print(courses)
+print(courses[2])
+print(courses[0])
 
 print('\n\n---Lab Challenge 2---')
-del Courses[0]
-del Courses[1]
-Courses.insert(1,"ET575")
-Courses.insert(2,"ET580")
-Courses.insert(3,"ET585")
-Courses.insert(4,"ET725")
+del courses[0]
+del courses[1]
+courses.insert(1,"ET575")
+courses.insert(2,"ET580")
+courses.insert(3,"ET585")
+courses.insert(4,"ET725")
 
-print(Courses)
-print(f'I am taking {len(Courses)} courses.')
+print(courses)
+print(f'I am taking {len(courses)} courses.')
 
 print('\n\n---Lab Challenge 3---')
-Courses.sort(reverse=True)
-print(Courses)
+courses.sort(reverse=True)
+print(courses)
 
 print('\n\n---Lab Challenge 4---')
 numbers = []
@@ -33,3 +33,13 @@ numbers.insert(2,2)
 numbers.insert(3,3)
 numbers.insert(4,4)
 print(numbers)
+
+save = numbers[2]
+numbers.remove(2)
+print(save)
+print(numbers)
+
+numbers.append(5)
+print(numbers)
+
+print('\n\n---Lab Challenge 5---')
