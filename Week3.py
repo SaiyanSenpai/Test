b9601b3dc047ad0622a7895d4e503cf0f66387ff
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-09-21 17:06:15 -0400

    Update Week3.py

--- Week3.py
+++ Week3.py
@@ -6,8 +6,18 @@ print(Courses)
 print(Courses[2])
 print(Courses[0])
 
-print('---Lab Challenge 2---')
+print('\n\n---Lab Challenge 2---')
 del Courses[0]
 del Courses[1]
+Courses.insert(1,"ET575")
+Courses.insert(2,"ET580")
+Courses.insert(3,"ET585")
+Courses.insert(4,"ET725")
+
 print(Courses)
 print(f'I am taking {len(Courses)} courses.')
+
+print('\n\n---Lab Challenge 3---')
+Courses.sort()
+Courses.sort(reverse=True)
+print(Courses)
