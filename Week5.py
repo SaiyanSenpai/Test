9f2fca2a893bbcf856a9ce6a0c9145508ca57942
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-10-12 17:16:38 -0400

    Update Week5.py

--- Week5.py
+++ Week5.py
@@ -30,3 +30,18 @@ if (num % 2) == 0 and  num > 50:
     print(f'{num} is a positive even integer greater than 50')
 elif (num % 2) != 0 and num<0:
     print(f'{num} is a negative odd integer')
+
+    
+print("\n4. ")
+final_grade = 96
+
+
+if final_grade >= 96 and final_grade <= 100:
+    print(f"{final_grade} is a grade of A")
+
+elif final_grade >= 64 and final_grade <=95:
+    print(f"{final_grade} is a grade of Passing")
+    
+elif final_grade < 64:
+    print(f"{final_grade} is a grade of Not passing")
+
