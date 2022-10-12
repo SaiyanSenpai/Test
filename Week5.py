cbc3c85e0ee30ee511fd33d51f176b83c59de420
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-10-12 17:01:41 -0400

    Update Week5.py

--- Week5.py
+++ Week5.py
@@ -1,8 +1,32 @@
-from telnetlib import theNULL
-
-
+print('1.')
 num = 10
-div_by_3 = num%5
+div_by_3 = num%3
+div_by_5 = num%5
 
 if div_by_3 != 0:
     print(f'{num} is not divisible by 3')
+else:
+    print(f'{num} is divisible by 3')
+
+if div_by_5 != 0:
+    print(f'{num} is not divisible by 5')
+else:
+    print(f'{num} is divisible by 5\n')
+
+print('2.')
+num = 12
+
+if num % 2 == 0:
+    print(f'{num} is divisible by 2')
+elif num % 4 == 0:
+    print(f'{num} is divisible by 4')
+else:
+    print(f'{num} is not divisible by 2 or 4')
+    
+print("\n3. ")
+num = 52
+
+if (num % 2) == 0 and  num > 50:
+    print(f'{num} is a positive even integer greater than 50')
+elif (num % 2) != 0 and num<0:
+    print(f'{num} is a negative odd integer')
