52f55eb42d208f1625b4a61fd1a2d0bd94104fe7
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-10-12 20:32:14 -0400

    Update Week5.py

--- Week5.py
+++ Week5.py
@@ -23,7 +23,7 @@ elif num % 4 == 0:
 else:
     print(f'{num} is not divisible by 2 or 4')
     
-print("\n3. ")
+print("\n3.")
 num = 52
 
 if (num % 2) == 0 and  num > 50:
@@ -32,7 +32,7 @@ elif (num % 2) != 0 and num<0:
     print(f'{num} is a negative odd integer')
 
     
-print("\n4. ")
+print("\n4.")
 final_grade = 96
 
 
@@ -41,7 +41,34 @@ if final_grade >= 96 and final_grade <= 100:
 
 elif final_grade >= 64 and final_grade <=95:
     print(f"{final_grade} is a grade of Passing")
-    
+
 elif final_grade < 64:
     print(f"{final_grade} is a grade of Not passing")
 
+
+print("\n5.")
+grades = [95, 63, 80, 53, 90, 56]
+
+passingGrades = [i for i in grades if i >=64]
+passingGrades_Average = sum(passingGrades)/len(passingGrades)
+
+total = 0
+for grade in grades:
+    if grade >= 64:
+        total = total+grade
+        passingGrades
+
+print(passingGrades_Average)
+
+print("\n6.")
+
+numbers = [n for n in range(1,31)]
+mult_of_3_and_5 = [n for n in numbers if n%3 == 0 or n%5 == 0]
+mult_of_3_and_not_5 = [n for n in numbers if n%3 == 0 and n%5 != 0]
+not_mult_of_3_and_5 = [n for n in numbers if n%3 == 0 and n%5 == 0]
+
+
+print(numbers)
+print(mult_of_3_and_5)
+print(mult_of_3_and_not_5)
+print(not_mult_of_3_and_5)
