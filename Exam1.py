4dae29158d5a73532fe7e94f471d1058ce66ef06
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-10-26 16:54:40 -0400

    Create Exam1.py

---
+++ Exam1.py
@@ -0,0 +1,39 @@
+# ET574 Python Exam 1   ### Tirtho Roy ###
+print ('Q1')
+names = ['Bird', 'Bryant', 'Ewing', 'Jordan', 'Maravich']
+print(sorted(names, reverse = True))
+
+print('\nQ2')
+name_list = ["Jordan", "Bird", "Bryant", "Maravich", "Ewing"]
+print(name_list)
+last_3_elements = slice(2,5)
+print(sorted(name_list[last_3_elements]))
+
+print('\nQ3')
+print('The 4 most popular programming languages of 2022 are:\n1.\tPython\n2.\tJavascript\n3.\tJava\n4.\tC#')
+
+print('\nQ4')
+temps = [56,65,47,74,38,83]
+print(min(temps),max(temps),(sum(temps)/(len(temps))))
+
+print('\nQ5')
+print((23/3)%1)
+
+print('Q6')
+pets = ['teddy','polly','jessy','ghost']
+
+print('Q7')
+
+print('Q8')
+
+print('Q9')
+
+print('Q10')
+
+print('Q11')
+
+print('Q12')
+
+print('Q13 extra-credit')
+
+print('Q14 extra-credit')
