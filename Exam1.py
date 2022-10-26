45a2b610ac9c7c07f0de3d2f8d1b6c551e426704
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-10-26 17:54:02 -0400

    Update Exam1.py

--- Exam1.py
+++ Exam1.py
@@ -19,21 +19,60 @@ print(min(temps),max(temps),(sum(temps)/(len(temps))))
 print('\nQ5')
 print((23/3)%1)
 
-print('Q6')
-pets = ['teddy','polly','jessy','ghost']
+print('\nQ6')
+pets = ['teddy','polly','jessie','ghost']
+print(f'Hello {str.upper(pets[pets.index("polly")])}, you are a fine parrot!')
 
-print('Q7')
+print('\nQ7')
+pets.insert(1,'spot')
+print(pets)
 
-print('Q8')
+print('\nQ8')
+pets_in_heaven = pets[pets.index('teddy'):pets.index('teddy')+1]+pets[pets.index('jessie'):pets.index('jessie')+1]
+print(pets_in_heaven)
 
-print('Q9')
+print('\nQ9')
+instruments = ['guitar','pinao','violin']
+for instrument in instruments:
+    print(f'I love to play the {instrument}')
 
-print('Q10')
+print('\nQ10')
+multiples = list(range(0,5*11,5))
+print(f'{multiples}\n{sum(multiples)}')
 
-print('Q11')
+print('\nQ11')
+gpa = 3.8
+def check_gpa(num):
+    if num>=3.5:
+        print("You are a great student!")
+    elif num>=2.5 and num<3.5:
+        print("You are a good student!")
+    else:
+        print("You really need to study more!")
+check_gpa(gpa)
 
-print('Q12')
+print('\nQ12')
+metals = ['tin', 'copper', 'brass', 'gold', 'iron']
+for metal in metals:
+    if metal == "gold":
+        print("Gold, Gold, I'm rich, I'm rich!")
+    else:
+        print(f"Darn {metal.title()} I have no luck")
 
-print('Q13 extra-credit')
+print('\nQ13 extra-credit')
+names=['Joe', 'Amy', 'Lil', 'Poe', 'Ali', 'Sid']
+grades=[65, 63, 89, 96, 59, 81] 
 
-print('Q14 extra-credit')
+for student_name in names:
+    index_num = names.index(student_name)
+    if grades[index_num] >=65:
+        print(f'{student_name.title()}, you passed!')
+    else:
+        print(f'{student_name.title()}, you need to study more!')
+
+print('\nQ14 extra-credit')
+fruits = ['apple','banana','cherry']
+descriptions = ['sweet','tasty']
+for taste in descriptions:
+    for fruit in fruits:
+        print(f'{taste} {fruit}')
