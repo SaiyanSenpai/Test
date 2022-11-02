a6946eb79ce03a60dedc00ef47423d1b65528bd4
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-11-02 17:09:41 -0400

    Update Week7.py

--- Week7.py
+++ Week7.py
@@ -5,11 +5,26 @@ while count<11:
     print(multiples[count], end = " ")
     count = count+1
     
-    
+#another way
+number = 0
+while number<=50:
+    print(number, end = " ")
+    number+=5
+
 
 
 print("\n2")
+grades = []
+while True:
+    prompt = "Enter a grade or -1 to stop: "
+    response = int(input(prompt))
+    if response == -1:
+        print(f'The GPA is {sum(grades)/len(grades):.0f}.')
+        break
+    else:
+        grades.append(response)
 
+    
 
 print("\n3")
 
