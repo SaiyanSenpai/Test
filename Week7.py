cd3af959f7a6e29f68216dd5d31678c87ea61e11
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-11-02 16:45:24 -0400

    1

---
+++ Week7.py
@@ -0,0 +1,18 @@
+print("1)")
+multiples = list(range(0,5*11,5))
+count = 0
+while count<11:
+    print(multiples[count], end = " ")
+    count = count+1
+    
+    
+
+
+print("\n2")
+
+
+print("\n3")
+
+print("\n4")
+
+print("\n5")
