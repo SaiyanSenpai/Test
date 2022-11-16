f27af08ed6a9db24964e9e5fecfa523e374e08a7
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-11-16 17:20:36 -0500

    Update Week9.py

--- Week9.py
+++ Week9.py
@@ -15,3 +15,12 @@ print(f'make={vehicle_3.make}\tmodel={vehicle_3.model}')
 print(f'make={vehicle_4.make}\tmodel={vehicle_4.model}')
 
 print("\n3")
+class Vehicle:
+    def __init__(self, make, model):
+
+class Boat(Vehicle):
+    def __init__(self, poweredBy):       # Init function has two parameters, n and g.
+        super().__init__(n)         # Call init of Person with the argument n.
+        self.gpa = float(g)         # Set value of gpa to g.
+
+print(Boat("nuclear"))
