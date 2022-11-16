e24a288db2c85f967aee7057c4109d36423a1639
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-11-16 17:01:55 -0500

    Update Week9.py

--- Week9.py
+++ Week9.py
@@ -1,6 +1,12 @@
 print("1)")
-class Vehicle():
-    make = "test"
+class Vehicle:
+    def __init__(car, make, model):
+        car.make = make
+        car.model = model
+vehicle_1 = Vehicle("Ford","Mustang")
+vehicle_2 = Vehicle("Toyota","Camry")
+print(vehicle_1.make)
+print(vehicle_2.model)
 
 print("\n2")
 
