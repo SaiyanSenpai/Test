8828d7067e50c265fa064f2552ea47986c5d853f
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-11-16 17:14:38 -0500

    Update Week9.py

--- Week9.py
+++ Week9.py
@@ -11,5 +11,7 @@ print(vehicle_2.model)
 print("\n2")
 vehicle_3 = Vehicle("Maserati","Levante")
 vehicle_4 = Vehicle("Porsche","944")
-print(vehicle_3)
+print(f'make={vehicle_3.make}\tmodel={vehicle_3.model}')
+print(f'make={vehicle_4.make}\tmodel={vehicle_4.model}')
+
 print("\n3")
