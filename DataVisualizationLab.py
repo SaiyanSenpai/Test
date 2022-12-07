b43e3de1b7d6acd67ea4dff3f40438d93e903932
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-12-07 17:11:41 -0500

    1

--- DataVisualizationLab.py
+++ DataVisualizationLab.py
@@ -29,4 +29,20 @@ import matplotlib.pyplot as plt
 # a Figure is the space upon which a graph can be placed
 fig = plt.figure()                  # create an empty figure
 fig = plt.figure(figsize=(9,6))    # create an empty figure which is 9 by by 6 inches
-plt.show()       
+plt.show()
+
+print("\n3)")
+# A Line plot
+#  
+# https://matplotlib.org/tutorials/introductory/usage.html#sphx-glr-tutorials-introductory-usage-py
+import matplotlib.pyplot as plt
+
+x = [1,2,3,4,5]
+y = [3,9,27,81,243]
+
+fix,ax = plt.subplots()
+ax.plot(x,y)                        # create a plot of x and y axis values
+ax.set_title('Powers of Three')     # set a title
+ax.set_xlabel('Numbers')            # name the x-axis
+ax.set_ylabel('Cubes')            # name the y-axis
+plt.show()
