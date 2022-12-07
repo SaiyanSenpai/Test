a33b9d5ae082884e882e846f7d2648232ff3a9a5
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-12-07 16:52:59 -0500

    1

--- DataVisualizationLab.py
+++ DataVisualizationLab.py
@@ -1,3 +1,8 @@
+'''
+    Use matploitlib.
+'''
+
+
 print("1)")
 import subprocess
 import sys
@@ -7,14 +12,15 @@ installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 
 all_packages_installed = True
 for n in ["matplotlib","pandas","plotly",'requests']:
-    
     if n in installed_packages:
         print(n+" is installed.")
         continue
     else:
         all_packages_installed = False
-        
 if all_packages_installed:
     print("The packages are installed.")
 else:
     print("All packages are not installed.")
+
+
+print("\n2)")
