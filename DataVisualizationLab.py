4e93d96d7f489597ef79cdb5377d4f7f63a58b67
SenpaiSaiyan <103065401+SaiyanSenpai@users.noreply.github.com>
2022-12-07 16:43:32 -0500

    Create DataVisualizationLab.py

---
+++ DataVisualizationLab.py
@@ -0,0 +1,19 @@
+print("1)")
+import subprocess
+import sys
+
+reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+all_packages_installed = True
+for n in ["matplotlib","pandas","plotly",'requests']:
+    
+    if n in installed_packages:
+        continue
+    else:
+        all_packages_installed = False
+        
+if all_packages_installed:
+    print("The packages are installed.")
+else:
+    print("All packages are not installed.")
